8d2ddd0d80d4dd572115df518208080981813e8c
javier perez osorio <japere@bancolombia.com.co>
2023-05-29 19:45:14 -0500

    Revert "Class 9 y 10"
    This reverts commit 0965d7e1b9ffadced10ebddd01664525077963cd.

--- src/topics/09-generics.ts
+++ src/topics/09-generics.ts
@@ -1,22 +1,3 @@
-export function whatsMytype<T>(argunment:any):T {
+export function whatsMytype(argunment:any) {
     return argunment;
 }
-
-
-// const amIsString = whatsMytype<string>('Hello');
-// const amIsNumber = whatsMytype<number>(1);
-// const amIsBoolean = whatsMytype<boolean>(true);
-
-const amIsString:string = whatsMytype('Hello');
-const amIsNumber:number = whatsMytype(1);
-const amIsBoolean:boolean = whatsMytype(true);    
-
-
-
-
-console.log(amIsString.split(''));
-console.log(amIsNumber.toFixed(2));
-console.log(amIsBoolean);
-
-
-
